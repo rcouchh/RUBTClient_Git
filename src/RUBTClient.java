945e1e13822e1212c53bd57715937cba5d32f654
Daniel Torres <dtorres315@gmail.com>
2014-10-01 21:06:22 -0400

    added tracker class

--- src/RUBTClient.java
+++ src/RUBTClient.java
@@ -1,13 +1,14 @@
 
 import java.io.*;
+import java.net.*;
 
 import GivenTools.*;
 
 /**
  *
  * @author Ryan Couch, Daniel Torres
- * 
- * 
+ *
+ *
  * Your assignment should basically do the following:
 
     1. Take as a command-line argument the name of the .torrent file to be loaded and the name of the file to save the data to. For example:
@@ -20,8 +21,8 @@ import GivenTools.*;
     6. After a piece is downloaded and verified, the peer is notified that you have completed the piece.
     7. Repeat steps 5-7 (using the same TCP connection) for the rest of the file.
     8. When the file is finished, you must contact the tracker and send it the completed event and properly close all TCP connections
-    9. Save the file to the hard disk according to the second command-line 
- * 
+    9. Save the file to the hard disk according to the second command-line
+ *
  */
 public class RUBTClient {
 
@@ -30,53 +31,56 @@ public class RUBTClient {
      */
     public static void main(String[] args) {
         // java -cp . RUBTClient somefile.torrent picture.jpeg
-    	if(args[0] == null | args[1] == null){
-    		System.out.println("Incorrect # of args!");
-    		System.exit(1);
-    	}
-    	
+        if(args[0] == null | args[1] == null){
+            System.out.println("Incorrect # of args!");
+            System.exit(1);
+        }
+
         String torr = args[0];
         String file = args[1];
-        
+
         byte[] torrentBytes = null;
-        
+
         try{
-       
+
         //open the .torrent file
-        final File torrentFile = new File(torr);
-        final DataInputStream torrentDataIn = 
-        		new DataInputStream(new FileInputStream(torrentFile));
-        		
+        final File torrentFile = new File("files/"+torr);
+        final DataInputStream torrentDataIn =
+                new DataInputStream(new FileInputStream(torrentFile));
+
         torrentBytes = new byte[(int)torrentFile.length()];
         torrentDataIn.readFully(torrentBytes);
         torrentDataIn.close();
-        
-        
+
+
         }catch (FileNotFoundException f){
-        	System.out.println("File not found!");
-        	System.exit(1);
+            System.out.println(torr);
+            System.out.println("File not found!");
+            System.exit(1);
         }catch (IOException e){
-        	System.out.println("IO Exception!");
-        	System.exit(1);
+            System.out.println("IO Exception!");
+            System.exit(1);
         }//end try
-        
+
         //if nothing loaded into torrentBytes
         if(torrentBytes==null){
-        	System.out.println("Torrent file corrupt! Unable to read.");
-        	System.exit(1);
+            System.out.println("Torrent file corrupt! Unable to read.");
+            System.exit(1);
         }
-        
+
         TorrentInfo torrentInfo = null;
         try{
-        	torrentInfo = new TorrentInfo(torrentBytes);
-        	
+            torrentInfo = new TorrentInfo(torrentBytes);
+
         }catch (final BencodingException b){
-        	System.out.println("Bencoding error! Unable to create TorrentInfo object.");
+            System.out.println("Bencoding error! Unable to create TorrentInfo object.");
         }
-        
-        
-        
-        
+
+        System.out.println("yous a bitch ass nigga");
+        System.out.println(torrentInfo.announce_url);
+        Tracker Track= new Tracker(torrentInfo);
+
+
     }//end main
-    
+
 }//end public class
---
+++ src/Tracker.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import GivenTools.TorrentInfo;
+
+
+public class Tracker extends RUBTClient{
+	final URL url;
+	final byte[] infoHash;
+	final int port ;
+	public Tracker(TorrentInfo Torr){
+		    this.url = (Torr.announce_url);
+		    this.port= this.url.getPort();
+		    this.infoHash= Torr.info_hash.array();
+		    
+	}
+	
+}
